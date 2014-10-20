8884fc7aa0e5fa4c334399667ee7fe2b7bbc6964
Anthony <aafetsrom@htghana.com>
2014-10-20 14:53:16 +0000

    taadded more views and some auth info

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -18,7 +18,53 @@ module.exports = {
 			}
 
 			res.redirect('/user');
-		}); 
+		});
+
+	}, 
+
+	auth: function(req, res, next){
+
+		if(!req.param('userName') || !req.param('passWord')){
+			req.session.flash = {
+				err: 'username and password are required for login'
+			}
+
+			res.redirect('/user/login');
+			return;
+		}
+
+		User.findOneByUserName(req.param('userName')).exec(function(err, user){
+			if(err) return next(err);
+
+			if(!user){
+				req.session.flash = {err: "account doesn't exist"}
+				res.redirect('user/login');
+				return;
+			}
+
+			require('bcrypt').compare(req.param('password'), user.passWord, function(err, valid){
+				if(err) return next(err);
+
+				if(!valid)
+				{
+					req.session.flash = { err: "username or password incorrect" }
+					res.redirect('/user/login');
+					return;
+				}
+
+				// user valid and can be authenticated
+				req.session.authenticated = true;
+				req.session.User = user;
+			});
+		});
+	},
+
+	login: function(req, res, next){
+		res.view();
+	}, 
+
+	index: function(req, res, next){
+		res.view();
 	}
 	
 };
